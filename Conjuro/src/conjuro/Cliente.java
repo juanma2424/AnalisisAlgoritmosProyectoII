8f2fe48556b8c6be28060478c5f73ff2a1bdee3d
juanma <42756436+juanma2424@users.noreply.github.com>
2019-05-28 21:31:01 -0600

    sockets server

--- Conjuro/src/conjuro/Cliente.java
+++ Conjuro/src/conjuro/Cliente.java
@@ -7,148 +7,190 @@ package conjuro;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.DataOutputStream;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import javax.swing.*;
 import java.net.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import static javax.swing.JComponent.TOOL_TIP_TEXT_KEY;
 
 public class Cliente {
-
+    
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
         MarcoCliente mimarco = new MarcoCliente();
-
+        
         mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        
     }
-
+    
 }
 
 class MarcoCliente extends JFrame {
-
+    
     public MarcoCliente() {
-
+        
         setBounds(600, 300, 280, 350);
-
+        
         LaminaMarcoCliente milamina = new LaminaMarcoCliente();
-
+        
         add(milamina);
-
+        
         setVisible(true);
+        addWindowListener(new EnvioOnline());
     }
-
+    
 }
 
-class LaminaMarcoCliente extends JPanel {
+class EnvioOnline extends WindowAdapter {
 
+    public void windowOpened(WindowEvent e) {
+        try {
+            Socket socketServidor = new Socket("192.168.150.1", 9999);
+            Empaque datos = new Empaque();
+            datos.setMns("online");
+            ObjectOutputStream paquetedatos = new ObjectOutputStream(socketServidor.getOutputStream());
+            paquetedatos.writeObject(datos);
+            paquetedatos.close();
+            
+        } catch (IOException ex) {
+            Logger.getLogger(EnvioOnline.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+}
+
+class LaminaMarcoCliente extends JPanel implements Runnable {
+    
     public LaminaMarcoCliente() {
-        campo2 = new JTextField(5);
-        add(campo2);
         
-        campo3 = new JTextField(8);
-        add(campo3);
+        String nick_user = JOptionPane.showInputDialog("Nick: ");
         
-
-        JLabel texto = new JLabel("Chat");
-
+        JLabel nickName = new JLabel("Nick: ");
+        add(nickName);
+        nick = new JLabel();
+        nick.setText(nick_user);
+        add(nick);
+        
+        ip = new JComboBox();
+        ip.addItem("u 1");
+        ip.addItem("u 2");
+        ip.addItem("u 3");
+        
+        JLabel texto = new JLabel("|Online");
+        
         add(texto);
-        at = new JTextArea(12,20);
-        add(at);
+        add(ip);
+        chat = new JTextArea(12, 20);
+        add(chat);
         campo1 = new JTextField(20);
-
+        
         add(campo1);
-
+        
         miboton = new JButton("Enviar");
-
+        
         EnviaTexto mi = new EnviaTexto();
         
         miboton.addActionListener(mi);
-
+        
         add(miboton);
-
+        Thread mihilo = new Thread(this);
+        mihilo.start();
+        
     }
-
+    
+    @Override
+    public void run() {
+        try {
+            ServerSocket sercli = new ServerSocket(9090);
+            Socket cliente;
+            Empaque paquete;
+            while (true) {
+                cliente = sercli.accept();
+                ObjectInputStream flojoEntrada = new ObjectInputStream(cliente.getInputStream());
+                paquete = (Empaque) flojoEntrada.readObject();
+                chat.append(paquete.getMns());
+            }
+            
+        } catch (IOException ex) {
+            Logger.getLogger(MarcoCliente.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MarcoCliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
     private class EnviaTexto implements ActionListener {
-
         
         @Override
         public void actionPerformed(ActionEvent e) {
             try {
                 // System.out.println("funk");
-                
+
                 // se crea socket
                 // dir ip
-                
-                
-                
-                Socket miso =new Socket("192.168.150.1",9999);
-                pak data = new pak();
-                data.setNick(campo2.getText());
-                data.setIp(campo3.getText());
+                Socket MiSocket = new Socket("192.168.150.1", 9999);
+                Empaque data = new Empaque();
+                data.setNick(nick.getText());
+                data.setIp((String) ip.getSelectedItem());
                 data.setMns(campo1.getText());
-                ObjectOutputStream  flujosalida= new ObjectOutputStream(miso.getOutputStream()); //flujo de salida
+                ObjectOutputStream flujosalida = new ObjectOutputStream(MiSocket.getOutputStream()); //flujo de salida
                 flujosalida.writeObject(data);
-                miso.close();
-                
+                MiSocket.close();
+
 //                DataOutputStream flujosalida = new  DataOutputStream(miso.getOutputStream());
 //                flujosalida.writeUTF(campo1.getText());
 //                flujosalida.close();
                 ////////////////////////////////
-                
-                
             } catch (IOException ex) {
                 System.out.println(ex.getMessage());
                 Logger.getLogger(LaminaMarcoCliente.class.getName()).log(Level.SEVERE, null, ex);
             }
             
         }
-      
-
         
     }
-
+    
     private JTextField campo1;
-    private JTextField campo2;
-    private JTextField campo3;
-     private JTextArea at;
-
+    private JLabel nick;
+    private JComboBox ip;
+    public JTextArea chat;
     private JButton miboton;
-
+    
 }
 
-
-class pak implements Serializable{
-    private String nick,ip,mns;
-
+class Empaque implements Serializable {
+    
+    private String nick, ip, mns;
+    
     public void setNick(String nick) {
         this.nick = nick;
     }
-
+    
     public void setIp(String ip) {
         this.ip = ip;
     }
-
+    
     public void setMns(String mns) {
         this.mns = mns;
     }
-
+    
     public String getNick() {
         return nick;
     }
-
+    
     public String getIp() {
         return ip;
     }
-
+    
     public String getMns() {
         return mns;
     }
-    
-    
     
 }
--- Conjuro/src/conjuro/Servidor.java
+++ Conjuro/src/conjuro/Servidor.java
@@ -13,8 +13,11 @@ import java.awt.event.ActionListener;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -59,27 +62,44 @@ class MarcoServidor extends JFrame implements Runnable {
     public void run() {
 
         try {
-            ServerSocket ns = new ServerSocket(9999);
+
+            ServerSocket socketConect = new ServerSocket(9999);
             String nick, ip, mns;
 
-            pak pac;
+            Empaque paquete;
 
             while (true) {
-                Socket ms = ns.accept();
-                ObjectInputStream pkdata = new ObjectInputStream(ms.getInputStream());//flujo
-                pac = (pak) pkdata.readObject();
 
-                nick = pac.getNick();
-                ip = pac.getIp();
-                mns = pac.getMns();
+                Socket miSocket = socketConect.accept();
+
+                ObjectInputStream DataPaquete = new ObjectInputStream(miSocket.getInputStream());//flujo
+                paquete = (Empaque) DataPaquete.readObject();
+
+                nick = paquete.getNick();
+                ip = paquete.getIp();
+                mns = paquete.getMns();
 
 //DataInputStream  fs = new DataInputStream(ms.getInputStream());
 //            String m = fs.readUTF();
-                areatexto.append("\n");
-                areatexto.append("\n" + nick);
-                areatexto.append("\n" + ip);
-                areatexto.append("\n" + mns);
-                ms.close();
+                if (!mns.equals("online")) {
+
+                    areatexto.append("\n" + nick + ", " + ip + ", " + mns);
+
+                    Socket enviadest = new Socket(ip, 9090);//server-cliente
+                    ObjectOutputStream pkrev = new ObjectOutputStream(enviadest.getOutputStream());//flujo de datos
+                    pkrev.writeObject(paquete);
+                    enviadest.close();
+
+                    miSocket.close();
+                } else {
+                    //-------------detec online
+                    InetAddress localizacion = miSocket.getInetAddress();
+
+                    String ipRemota = localizacion.getHostAddress();
+
+                    System.out.println("Ip " + ipRemota);
+                    ArrayList <String> listaIP = new ArrayList<String>(); 
+                }
             }
         } catch (IOException ex) {
             Logger.getLogger(MarcoServidor.class.getName()).log(Level.SEVERE, null, ex);
